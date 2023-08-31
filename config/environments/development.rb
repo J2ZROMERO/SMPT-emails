7b88ae05ca2712452a2eea2c588b12baae6138aa
Jose Zepeda <jose-zromero@hotmail.com>
2023-08-30 18:54:05 -0600

    Missing controllers

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,9 +37,22 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'google.com', protocol: 'https' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'google.com',
+    user_name:            'j0s3jzr@gmail.com',
+    password:             'rczvjgzfhewvdiyb',
+    authentication:       'plain',
+    enable_starttls_auto: true,
+    open_timeout:         5,
+    read_timeout:         5 }
+    config.action_mailer.perform_deliveries = true
+    config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.default_options = {from: 'no-reply@example.com'}
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
