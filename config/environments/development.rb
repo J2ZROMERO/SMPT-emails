7a02c93cd4126e7cd0659247686accbc4d904b7c
Jose Zepeda <jose-zromero@hotmail.com>
2023-09-04 19:53:12 -0600

    Add credentials for smtp aws

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,12 +41,12 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'gmail.com', protocol: 'https' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
+    address:              'email-smtp.us-east-1.amazonaws.com',
     port:                 587,
     domain:               'gmail.com',
-    user_name:            'j0s3jzr@gmail.com',
-    password:             'rczvjgzfhewvdiyb',
-    authentication:       'plain',
+    user_name:            'AKIAXMAPCONIDG53VFV7',
+    password:             'BEBOeenFcpawFqUH1eHn5Y8PfBVdKQdgv0Yl+fIaps1b',
+    authentication:       :plain,
     enable_starttls_auto: true,
     open_timeout:         5,
     read_timeout:         5 }
